3b833d961abede8afddf342a03ec50228fb32db9
Александр Яцевский <sashayatsevskiy@ukr.net>
2020-07-25 17:30:46 +0300

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store.component';
+import {StoreModule} from './store/store.module';
+import {CartDetailComponent} from './store/cartDetail.component';
+import {CheckoutComponent} from './store/checkout.component';
+import {StoreFirstGuard} from './storeFirst.guard';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule,
+    RouterModule.forRoot([
+      {
+        path : "store", component : StoreComponent,
+        canActivate : [StoreFirstGuard]
+      },
+      {path: 'cart', component: CartDetailComponent,
+        canActivate : [StoreFirstGuard]},
+      {path: "checkout", component: CheckoutComponent,
+        canActivate : [StoreFirstGuard]},
+      {path: "**", redirectTo : "/store"}
+    ])
   ],
-  providers: [],
+  providers: [StoreFirstGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
